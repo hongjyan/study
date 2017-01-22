8e6b7a573628c13580028bdd137b0b21df2f2ab2
NexAlien <hongjun.yang@nokia.com>
2017-01-22 11:20:39 +0800

    add sleep in main.cpp

--- cpp/thread/main.cpp
+++ cpp/thread/main.cpp
@@ -1,14 +1,28 @@
 #include "stub.hpp"
 #include "lib.hpp"
 #include <iostream>
+#include <sys/wait.h>
+#include <unistd.h>
+
+
+void printName(void* stub) {
+     Stub* pstub = (Stub*)stub;
+     std::cout << pstub->getName() << std::endl;
+}
 
 int main() {
     Stub stub;
     stub.setName("xiaoming");
     Lib lib;
+    //pthread_cleanup_push(printName, &stub);
 
     lib.start(1, stub.handler);
+
+    sleep(1);
+    printName(&stub);
     
-    std::cout << stub.getName() << std::endl;
+    //pthread_exit(0);
+   // pthread_cleanup_pop(0);
+   return 0;
 }
     
