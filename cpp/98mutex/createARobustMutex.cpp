24f7c8d9c506e4ccce2cb62a30a046921e328772
hongjyan <hongjun.yang@nokia.com>
2017-03-09 08:36:46 +0200

    init a robust mutex

---
+++ cpp/98mutex/createARobustMutex.cpp
@@ -0,0 +1,24 @@
+#include <pthread.h>
+#include <cstdio>
+#include <string.h>
+
+
+int main() {
+    pthread_mutexattr_t attr;
+    pthread_mutex_t mutex;
+
+    if (pthread_mutexattr_setrobust(&attr, PTHREAD_MUTEX_ROBUST)) {
+        printf("set mutex robust attribute failed, exit\n");
+        return 0;
+    }
+
+
+    if (pthread_mutex_init(&mutex, &attr)) {
+        printf("init mutex failed.\n");
+        return 0;
+    }
+
+
+    printf("init a robust mutex success\n");
+    return 0;
+}
