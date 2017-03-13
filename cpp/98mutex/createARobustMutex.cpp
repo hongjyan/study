056b3739c91a5c3cfefe2e95e8c9de09fb0128a9
hongjyan <hongjun.yang@nokia.com>
2017-03-13 07:44:45 +0200

    update 98mutex

--- cpp/98mutex/createARobustMutex.cpp
+++ cpp/98mutex/createARobustMutex.cpp
@@ -7,7 +7,7 @@ int main() {
     pthread_mutexattr_t attr;
     pthread_mutex_t mutex;
 
-    if (pthread_mutexattr_setrobust(&attr, PTHREAD_MUTEX_ROBUST)) {
+    if (pthread_mutexattr_init(&attr) || pthread_mutexattr_setrobust(&attr, PTHREAD_MUTEX_ROBUST)) {
         printf("set mutex robust attribute failed, exit\n");
         return 0;
     }
