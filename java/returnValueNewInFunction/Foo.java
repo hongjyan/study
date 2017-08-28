0b6f4752a86ea3cb158828ffc74f755b4c28e157
hongjyan <hongjun.yang@nokia-sbell.com>
2017-08-28 10:25:25 +0300

    update VInF.java

---
+++ java/returnValueNewInFunction/Foo.java
@@ -0,0 +1,11 @@
+public class Foo {
+    public Foo(int i) {
+        i_ = i;
+    }
+
+    public int getAttribute() {
+        return i_;
+    }
+
+    private int i_;
+};
--- java/returnValueNewInFunction/VInF.java
+++ java/returnValueNewInFunction/VInF.java
@@ -1,13 +1,21 @@
 public class VInF {
     public static String[] F() {
-	String[] str = new String[2];
-	str[0] = "Hello";
-	str[1] = "World";
-	return str;
+        String[] str = new String[2];
+        str[0] = "Hello";
+        str[1] = "World";
+        return str;
+    }
+
+    public Foo getFoo() {
+        Foo foo = new Foo(1);
+        return foo;
     }
 
     public static void main(String[] args) {
-	String[] strout = F();
-	System.out.println(str[0] + " " + str[1]);
+        String[] strout = F();
+        System.out.println(strout[0] + " " + strout[1]);
+        
+        VInF vinf = new VInF();
+        System.out.println(vinf.getFoo().getAttribute());
     }
 }
