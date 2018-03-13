e05c83309408c159ecc6b8acd31528328fab7a90
hongjyan <hongjun.yang@nokia-sbell.com>
2018-03-13 14:45:28 +0800

    new bash directory

---
+++ bash/test.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/bash
+game='fun'
+eclname='game'
+eclpath="${!eclname}" #!var, take the value of variable as new variable name 
+echo ${eclpath}
+echo -e "-----------------------------------\n\n"
+
+#2018/3/13
+var="have fun"
+echo "= should be used with \"test\" command for POSIX conformance."
+echo "STRING1 = STRING2
+	the strings are equal"
+test "$var" = "have fun" && echo have fun
+test "$var" = "have\sfun" && echo have\\sfun
+echo -e "-----------------------------------\n\n"
+
+
+echo "== is not defined by POSIX, it's an extension of bash, derived from ksh, should not use == when want portability. =~ is recomended in bash."
+echo "string1 == string2
+    True if the strings are equal. When used with the [[ command, this performs pattern matching"
+set +x
+#character matching
+[[ $var == "have fun" ]] && echo var matches "have fun"
+bar="hav"
+[[ $bar == "have fun" ]] && echo bar match \"have fun\"
+
+#pattern matching
+[[ $var == have\sfun ]] && echo have\\sfun #\s\t\n can not use in bash regex, refer to "man 7 regex" to know more. they should be expressed as [[:class:]], class is one of "alnum   digit   punct alpha   graph   space blank   lower   upper cntrl   print   xdigit".
+[[ $var == have[[:space:]]+fun ]] && echo == have[[:space:]]+fun #should work, but not.
+[[ $var =~ have[[:space:]]+fun ]] && echo =~ have[[:space:]]+fun #work, recommend to use =~ when do matching
+[[ $var == have[[:space:]]*fun ]] && echo == have[[:space:]]*fun # * work in ==, but + won't, don't know why.
+[[ $var =~ have[[:space:]]*fun ]] && echo =~ have[[:space:]]*fun
+[[ $var == have.*fun ]] && echo == have.*fun
+[[ $var =~ have.*fun ]] && echo =~ have.*fun
+[[ $var == have* ]] && echo == have*
+[[ $var =~ have* ]] && echo =~ have*
+
+#wrong usage. will be interpreted as "[[ -n have fun==havesfun ]]"
+[[ $var==have\sfun ]] && echo  have\\sfun    
+set +x
+echo -e "-----------------------------------\n\n"
+
