54c6e67711ca677499c9bb624fea906b5aae3053
hongjyan <hongjun.yang@nokia-sbell.com>
2020-10-22 06:25:56 +0300

    unique_ptr and dynamic array memory

--- cpp/unique_ptr/test.cpp
+++ cpp/unique_ptr/test.cpp
@@ -2,16 +2,21 @@
 #include <memory>
 using namespace std;
 
-int main() {
-  typedef int arry10[10];
-  unique_ptr<int[]> up(new arry10{1, 2, 3, 4}); //recommend way to create weak_ptr
-  
-  int *p = new arry10{11, 22, 33};
-  unique_ptr<int[]> up2(p);
+class Foo {
+public:
+  Foo() { cout << "ctr" << endl; }
+  ~Foo() { cout << "dctr" << endl; }
+};
 
-  up2.release();
-  /* test if up.release has release dynamic array or not */
-  cout << *(p+1) << endl; //coredump if up2.release() do release memory, but seems not.
+int main() {
+  unique_ptr<Foo[]> up(new Foo[10]());
+  //up.release();  //wrong, will lead memory leak
+  up.reset();  //can use reset to call delete on Foo[10]
+  //delete [] p;  //coredump. delete Foo[10] twice
+  cout << "---------------------------" << endl;
   
+  unique_ptr<Foo[]> up2(new Foo[10]()); //unique_ptr will delete the memory it points when out of its life.
+  cout << "---------------------------" << endl;
+
   return 0;
 }
