c004bb7e05b03b8a85d51ed26c5790f90981578a
hongjyan <hongjun.yang@nokia-sbell.com>
2021-02-01 05:11:10 +0200

    new file:   template_general.cpp

---
+++ cpp/template/template_general.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+
+#include <type_traits>
+#include <typeinfo>
+#ifndef _MSC_VER
+#   include <cxxabi.h>
+#endif
+#include <memory>
+#include <string>
+#include <cstdlib>
+
+template <class T>
+std::string
+type_name()
+{
+    typedef typename std::remove_reference<T>::type TR;
+    std::unique_ptr<char, void(*)(void*)> own
+           (
+#ifndef _MSC_VER
+                abi::__cxa_demangle(typeid(TR).name(), nullptr,
+                                           nullptr, nullptr),
+#else
+                nullptr,
+#endif
+                std::free
+           );
+    std::string r = own != nullptr ? own.get() : typeid(TR).name();
+    if (std::is_const<TR>::value)
+        r += " const";
+    if (std::is_volatile<TR>::value)
+        r += " volatile";
+    if (std::is_lvalue_reference<T>::value)
+        r += "&";
+    else if (std::is_rvalue_reference<T>::value)
+        r += "&&";
+    return r;
+}
+
+
+
+template <typename T> void func(T val) {
+    cout << type_name<decltype(val)>() << endl;  //still be int, since func(const int) is the same with func(int)
+}
+
+int main() {
+    const int i = 10;
+    cout << type_name<decltype(i)>() << endl;
+    func(i);
+    return 0;
+}
