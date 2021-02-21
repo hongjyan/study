355f12f229df5a7f01e4696f6b8ce36907d8f4eb
hongjyan <hongjun.yang@nokia-sbell.com>
2021-02-21 12:19:58 +0200

    new file:   manipulator.cpp

---
+++ cpp/IO/manipulator.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+  //cout manipulator
+  cout << "default bool value: " << true << " " << false
+       << "\nalpha bool value: " << boolalpha << true << " " << false << endl;
+  
+  cout << showbase;
+  cout << "default: " << 20 << " " << 1024 << endl;
+  cout << "octal:" << oct << 20 << " " << 1024 << endl;
+  cout << "octal still: " << 20 << " " << 1024 << endl;
+  cout << "hex:" << hex << 20 << " " << 1024 << endl;
+  cout << "decimal:" << dec << 20 << " " << 1024 << endl;
+
+  cout << noshowbase;
+  cout << "hex:" << hex << 20 << " " << 1024 << endl;
+
+  cout << showbase << uppercase << hex
+       << "print in upper hex:" << 20 << " " << 1024 
+       << noshowbase << nouppercase << dec << endl;
+
+  int i = -16;
+  double d = 3.14159;
+  
+  cout << left;
+  cout << "i:" << "  " << setw(12) << i << "  " << "next col" << endl;  //setw won't change IO state as endl;
+  cout << "d:" << "  " << setw(12) << d << "  " << "next col" << endl;
+  cout << right; //restore to default right
+
+
+  //cin manipulator
+  char ch;
+  while (cin >> ch) {  //space will be omitted in default.
+    cout << ch;
+  }
+
+  cin.clear();
+  cout << "again, keep space";
+
+  cin >> noskipws;
+  while (cin >> ch) {
+    cout << ch;
+  }
+  return 0;
+}
