55a90617762e73576367e3b45a7f3507574dd71f
hongjyan <hongjun.yang@nokia-sbell.com>
2023-03-08 07:37:45 +0200

    classIncludeSelf

---
+++ cpp/class/classIncludeSelf.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class foo;
+
+// error: â€˜fâ€™ has incomplete type
+// foo func(foo f){
+//     return f;
+// }
+
+foo* func2(foo* pf){
+    return pf;
+}
+
+struct Bar
+{
+    Bar() { std::cout << "ctor" << std::endl; }
+
+    Bar getDefaultBar()
+    {
+        return Bar();
+    }
+
+private:
+    // Bar self_; // will cause Bar infinitely large
+    Bar* pself_;  // ok
+};
+
+
+int main() {
+    Bar bar;
+    bar.getDefaultBar();
+    return 0;
+}  
