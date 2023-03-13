f5c2b5083ead3852cdf187a3832f9dd22ee384b3
hongjyan <hongjun.yang@nokia-sbell.com>
2023-03-13 10:42:41 +0200

    privateMemberInBothBaseAndDerived

--- cpp/class/privateMemberInBothBaseAndDerived.cpp
+++ cpp/class/privateMemberInBothBaseAndDerived.cpp
@@ -1,25 +1,75 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class base {
 public:
+    base() : s_("1"), s2_("11"), s3_("111") {}
+    // 如果函数非virtual， 一律使用基类内容
+    // 如果函数为virtual， 只有派生类重定义了，那么才使用派生类内容
     void output() {
-        cout << getVal() << endl;
+        cout << vGetStr() << endl;
+        cout << vGetStr2() << endl;
+        cout << getStr3() << endl;
+        cout << vGetStr3() << endl;
     }
 
 private:
-    int getVal()
+    virtual string vGetStr()
     {
-        return val_;
+        cout << __func__ << ": ";
+        return s_;
     }
-    int val_{1};
+
+public:
+    virtual string vGetStr2()
+    {
+        cout << __func__ << ": ";
+        return s2_;
+    }
+
+    string getStr3()
+    {
+        cout << __func__ << ": ";
+        return s3_;
+    }
+    virtual string vGetStr3()
+    {
+        cout << __func__ << ": ";
+        return s3_;
+    }
+
+private:
+    string s_{"1"}, s2_{"11"}, s3_{"111"};
 };
 
 class derived : public base {
-    int val_{2};
+public:
+    derived() : s_("2"), s2_("22"), s3_("222") {}
+
+private:
+     string vGetStr()
+    {
+        cout << __func__ << ": ";
+        return s_;
+    }
+public:
+    string getStr3()
+    {
+        cout << __func__ << ": ";
+        return s3_;
+    }
+    string vGetStr3()
+    {
+        cout << __func__ << ": ";
+        return s3_;
+    }
+
+private:
+    string s_{"2"}, s2_{"22"}, s3_{"222"};
 };
 
 int main() {
     derived d;
-    d.output(); // 注意： 打印1. 二不是2.
+    d.output();
 }
